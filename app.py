fdd232a7ccb7d798e2ceda7761fe8d806138f3cd
zeppelino <mauricioacdc93@gmail.com>
2025-04-22 00:13:40 -0300

    cambios de estilos

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('home.html')  # Esto busca el archivo en /templates
+    return render_template('base.html')  # Esto busca el archivo en /templates
 
 
 if __name__ == '__main__':
