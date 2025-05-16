689ce26a07fbcd594c144815844513c14ee8255d
zeppelino <mauricioacdc93@gmail.com>
2025-05-15 23:17:29 -0300

    m

--- app.py
+++ app.py
@@ -1,52 +1,14 @@
-# from flask import Flask, render_template, url_for
-
-# app = Flask(__name__)
-
-
-# @app.route('/')
-# def home():
-#     return render_template('base.html')  # Esto busca el archivo en /templates
-
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-# # RUTAS PROVISORIAS
-
-
-# @app.route('/stock')
-# def stock():
-#     return render_template('stock.html')
-
-
-# @app.route('/venta')
-# def venta():
-#     return render_template('venta.html')
-
-
-# @app.route('/informe')
-# def informe():
-#     return render_template('informe.html')
-
-
-# @app.route('/usuarios')
-# def usuarios():
-#     return render_template('usuarios.html')
-
-# @app.route('/login')
-# def login():
-#     return render_template('login.html')
-
-from flask import Flask, render_template, url_for
-
-from dotenv import load_dotenv
-load_dotenv()
+from flask import Flask,render_template
 
 from routes.stock import stock_bp
 from routes.venta import venta_bp
 from routes.usuario import usuario_bp
 from routes.informe import informe_bp
 from routes.login import login_bp
+
+from dotenv import load_dotenv
+load_dotenv()
+
 
 
 app = Flask(__name__)
--- routes/venta.py
+++ routes/venta.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from controllers.stock import obtener_productos
+from controllers.venta import cargar_venta
 
 venta_bp = Blueprint('venta', __name__)
 
+############## Ruta de ventana original
 @venta_bp.route('/venta')
 def venta():
-    return render_template('venta.html')
+    """ Ruta que lleva a la ventana venta con los productos """
+    productos = obtener_productos()
+    return render_template('venta.html', productos=productos)
+
+############## Ruta agregar una venta
+@venta_bp.route('/venta/pedido')
+def cargar_venta_route():
+    """ Ruta que carga un pedido a la base """
+    pedido = cargar_venta()
+    return True
