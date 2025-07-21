2db04904b61cf93d61d385ccf221e3b33a93e6da
zeppelino <mauricioacdc93@gmail.com>
2025-07-21 15:42:05 -0300

    .

--- tests/test_usuario.py
+++ tests/test_usuario.py
@@ -18,25 +18,9 @@ def test_cargar_usuario_exitoso(client):
     # Verifica que la redirección fue exitosa
     assert response.status_code == 200
 
-    # Verifica que el mensaje de éxito esté presente en la respuesta
-    assert b'ok' in response.data
+    # Verifica que el mensaje esté en la respuesta
+    assert b'OK' in response.data
     
-# def test_cargar_usuario_error_dni(client):
-#     # Datos con DNI inválido
-#     data = {
-#         'dni': '1234', # dni corto deberia fallar y no devolcer un status 200
-#         'nombre': 'Juan Pérez',
-#         'fecha_nacimiento': '2000-01-01',
-#         'puesto': 'cajero',
-#         'estado': 'activo',
-#         'clave': 'Clave123'
-#     }
-
-#     # Ejecuta la solicitud POST a la ruta
-#     response = client.post('/usuario/cargar', data=data, follow_redirects=True)
-
-#     # Verificao que el mensaje de error esté  en la respuesta
-#     assert b'El DNI ingresado no es v\xc3\xa1lido' in response.data
     
 def test_verificar_clave(client):
     # Datos con clave inválida (sin mayúsculas)
